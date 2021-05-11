8864014c1172aadca7a2309a82c723c5d9a9334c
Petar Ilic <petarilic49@gmail.com>
2021-05-11 14:09:34 -0400

    added ignore

--- Path_Planning.py
+++ Path_Planning.py
@@ -1,1 +1,12 @@
-print('test')
+#GOAL: the goal of this project is to compare different path planning algorithms
+#Two fo the algorithms are popular in robot navigation (1. Potential Field 2. Dynamic Window Approach)
+#The other two are grid searches for most optimal path from two distances (1. A* Algorithm 2. Dijkstra Algorithm)
+
+import pygame
+import numpy as np
+import pandas as pd
+
+def Dijkstra(): 
+    print('lets begin')
+
+Dijkstra()

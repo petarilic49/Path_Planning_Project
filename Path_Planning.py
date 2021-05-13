ef13922d04e4027c3d8d9928e355f3321f029ffa
Petar Ilic <petarilic49@gmail.com>
2021-05-12 23:00:51 -0400

    almost there with dij

--- Path_Planning.py
+++ Path_Planning.py
@@ -28,6 +28,8 @@ class Node():
         self.color = WHITE
         self.neighbor = []
         self.size = size
+        self.source = False
+        self.destination = False
     
     def get_pos(self):
         return self.row, self.col
@@ -37,6 +39,28 @@ class Node():
         self.color = RED
     def set_barrier(self):
         self.color = BLACK
+    def is_source(self):
+        self.source = True
+    def is_destination(self):
+        self.destination = True
+    def visited(self):
+        self.color = YELLOW
+    def was_visited(self):
+        return self.color == YELLOW
+    # This function can tell us which node to visit next 
+    def update_neighbor(self, grid): #This function lets every node hold its neighbors for easier computation
+        if self.col > 0: 
+            self.neighbor.append(grid[self.row][self.col - 1]) #Up
+        if self.col < 23:
+            self.neighbor.append(grid[self.row][self.col + 1]) #Down
+        if self.row < 23:
+            self.neighbor.append(grid[self.row + 1][self.col]) #Right
+        if self.row > 0:
+            self.neighbor.append(grid[self.row - 1][self.col]) #Left
+    def get_neighbor(self):
+        return self.neighbor
+    def neighbor_num(self):
+        return len(self.neighbor)
     def draw(self, scn):
         pygame.draw.rect(scn, self.color, pygame.Rect(self.x, self.y, self.size, self.size))
         
@@ -62,20 +86,29 @@ def grid_loop(rows, cols):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if pygame.mouse.get_pressed() == (1, 0, 0): #makes sure that only the left mouse click works 
+            if pygame.mouse.get_pressed() == (1, 0, 0): #makes sure that only the left mouse click works, well use the same syntax but right click to start the algorithm 
                 mousepos = pygame.mouse.get_pos()
                 i, j = mousepos
                 i = i // size
                 j = j // size
                 node = nodes[i][j]
                 if mouseclick == 0:
+                    node.is_source()
                     node.set_source()
+                    start = node
                     mouseclick = 1
                 elif mouseclick == 1:
+                    node.is_destination()
                     node.set_destination()
+                    end = node
                     mouseclick = 2
                 else:
                     node.set_barrier()
+            elif pygame.mouse.get_pressed() == (0, 0, 1): #if the right mouse button is clicked start the algorithm
+                dijkstra(nodes, start, end)
+                
+    
+        
                 
 
 #Function to draw the grid (ie horizontal and vertical lines)
@@ -110,4 +143,79 @@ def drawGrid(grid, rows, cols):
     draw_gridlines(rows, cols)
     pygame.display.update()
 
+#What it needs to do: 
+# - Start at the start node and initialize a distance of 0
+# - Initialize all other nodes in the grid to have a distance of infinity
+# - Take the neighbor of the start node and update the distance to current distance plus 1
+# - Repeat this for the each neighbor node
+def dijkstra(grid, start, end):
+    #Get the row and col of the start node
+    start_row, start_col = start.get_pos()
+    # Get the row and col of the end node
+    end_row, end_col = end.get_pos()
+    #For now just set infinity to very high number
+    inf = 1000000000
+    #Make an array/list that has infinity for all the distances
+    distance = []
+    for i in range(len(grid) - 1):
+        distance.append([])
+        for j in range(len(grid) - 1):
+            distance[i].append(inf)
+
+    #Get the position of the start node and make the distance equal to 0
+    distance[start_row][start_col] = 0
+
+    #Create a list for the visited and unvisisted nodes (just holds the row and col of the nodes visited)
+    visited = []
+    for i in range(len(grid) - 1):
+        visited.append([])
+        for j in range(len(grid) - 1):
+            visited[i].append((False))
+    unvisited = []
+    for i in range(len(grid) - 1):
+        unvisited.append([])
+        for j in range(len(grid) - 1):
+            unvisited[i].append((i, j))
+
+    #Append the starting node to the visited list and pop the start node from the unvisited list (For some reason Pygame assumed rows is top and columns is side)
+    #visited.append(unvisited[start_row].pop(start_col))
+    visited[start_row][start_col] = True
+
+    #Make the distance of the starting nodes neighbors equal to 1 and change their color 
+    for row in grid:
+        for node in row:
+            node.update_neighbor(grid)
+
+    current_node = start
+    #Were gonna go through each nodes neighbors and update the distance
+    finished = False
+    while not finished:
+        current_row, current_col = current_node.get_pos() 
+        #Move one up
+        if current_col > 0: #Check the distance of the up node
+            if distance[current_row][current_col - 1] > distance[current_row][current_col] + 1 and not visited[current_row][current_col - 1]:
+                distance[current_row][current_col - 1] = distance[current_row][current_col] + 1
+                grid[current_row][current_col - 1].visited()
+        if current_col < 23: #Check the distance of the down node
+            if distance[current_row][current_col + 1] > distance[current_row][current_col] + 1 and not visited[current_row][current_col + 1]:
+                distance[current_row][current_col + 1] = distance[current_row][current_col] + 1
+                grid[current_row][current_col + 1].visited()
+        if current_row < 23: #Check the distance of the right node
+            if distance[current_row + 1][current_col] > distance[current_row][current_col] + 1 and not visited[current_row + 1][current_col]:
+                distance[current_row + 1][current_col] = distance[current_row][current_col] + 1
+                grid[current_row + 1][current_col].visited()
+        if current_col < 23: #Check the distance of the left node
+            if distance[current_row - 1][current_col] > distance[current_row][current_col] + 1 and not visited[current_row - 1][current_col + 1]:
+                distance[current_row - 1][current_col] = distance[current_row][current_col] + 1
+                grid[current_row - 1][current_col].visited()
+        
+        visited[current_row][current_col] = True
+        #Now to somehow have to check which node should be visited next. It should be the smallest distance
+        min_value = min(distance)
+        print(min_value)
+        min_index = distance.index(min_value)
+        print(min_index)
+        #print(distance)
+        finished = True
+        #current_node = grid
 grid_loop(25, 25)
